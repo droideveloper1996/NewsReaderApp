421e35f6c252953cfa28e8bfb15979f7b06e8cef
droideveloper1996 <droid.developer1996@gmail.com>
2017-03-09 22:09:37 +0530

    Modified MainActivity Added Test Class Added Network Check Includes Assets Modified Layout to include images for connectivity

--- app/src/main/java/scorecard/project/com/newsreaderapp/MainActivity.java
+++ app/src/main/java/scorecard/project/com/newsreaderapp/MainActivity.java
@@ -1,8 +1,11 @@
 package scorecard.project.com.newsreaderapp;
 
 import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -12,8 +15,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -23,8 +28,10 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<A
     private ListView mListView;
     private NewsAdapter newsAdapter;
     private ArrayList<News> news;
+    private ImageView mImageView;
     private TextView mEmptyTExtView;
     private ProgressBar progressBar;
+    private RelativeLayout relativeLayout;
     private SwipeRefreshLayout swipeRefreshLayout;
     private String URL_TO_FETCH = "https://content.guardianapis.com/search?q=cricket&order-by=newest&api-key=24e0000b-55d7-4c59-8994-44471580c508";
 
@@ -37,13 +44,26 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<A
         progressBar = (ProgressBar) findViewById(R.id.progressBar1);
         mListView = (ListView) findViewById(R.id.listView);
         mEmptyTExtView = (TextView) findViewById(R.id.empty_text_view);
+        mImageView = (ImageView) findViewById(R.id.empty_image_view);
         news = new ArrayList<News>();
         newsAdapter = new NewsAdapter(this, news);
         mListView.setAdapter(newsAdapter);
         getSupportActionBar().hide();
         loaderManager = getLoaderManager();
-        loaderManager.initLoader(1, null, this);
         mListView.setEmptyView(mEmptyTExtView);
+        mListView.setEmptyView(mImageView);
+        relativeLayout = (RelativeLayout) findViewById(R.id.activity_main);
+
+        if (isConnected()) {
+
+            loaderManager.initLoader(1, null, this);
+
+        } else {
+            mEmptyTExtView.setText(R.string.no_internet_connectivity);
+            mEmptyTExtView.setPadding(0, 15, 0, 0);
+            mImageView.setImageResource(R.drawable.noconnectivity);
+            relativeLayout.setBackgroundColor(getResources().getColor(R.color.connectivityColor));
+        }
         mListView.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -52,6 +72,25 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<A
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse(url));
                 startActivity(intent);
+            }
+        });
+        swipeRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.i("onRefresh Triggered", "Fetching data");
+
+                newsAdapter.clear();
+                if (isConnected()) {
+                    mImageView.setVisibility(View.GONE);
+                    mEmptyTExtView.setVisibility(View.GONE);
+                    loaderManager.restartLoader(1, null, MainActivity.this);
+                } else {
+                    mImageView.setImageResource(R.drawable.noconnectivity);
+                    relativeLayout.setBackgroundColor(getResources().getColor(R.color.connectivityColor));
+                    mEmptyTExtView.setText(R.string.no_internet_connectivity);
+                    mEmptyTExtView.setVisibility(View.VISIBLE);
+                }
+                swipeRefreshLayout.setRefreshing(false);
             }
         });
     }
@@ -68,22 +107,24 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<A
         if (data != null && !data.isEmpty()) {
             newsAdapter.addAll(data);
         } else {
+            mImageView.setImageResource(R.drawable.noresult);
+            relativeLayout.setBackgroundColor(getResources().getColor(R.color.background));
             mEmptyTExtView.setText(R.string.no_data_found);
         }
-        swipeRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                Log.i("onRefresh Triggered", "Fetching data");
-                newsAdapter.clear();
-                loaderManager.restartLoader(4, null, MainActivity.this);
-                swipeRefreshLayout.setRefreshing(false);
-            }
-        });
-
+        swipeRefreshLayout.setRefreshing(false);
     }
 
     @Override
     public void onLoaderReset(Loader<ArrayList<News>> loader) {
         newsAdapter.clear();
     }
+
+    public boolean isConnected() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
+
+    }
+
+
 }
